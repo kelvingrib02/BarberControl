40d94cc9212c26c4c2f397b90e06421858b86954
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-12 01:11:20 -0300

    Construindo CRUD clientes

---
+++ src/Barbearia.Api/Controller/ClientesController.cs
@@ -0,0 +1,87 @@
+﻿using Barbearia.Domain.Entities;
+using Barbearia.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barbearia.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientesController : ControllerBase
+{
+    private readonly BarbeariaDbContext _context;
+
+    public ClientesController(BarbeariaDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+    {
+        return await _context.Clientes.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Cliente>> GetCliente(int id)
+    {
+        var cliente = await _context.Clientes.FindAsync(id);
+
+        if (cliente == null)
+            return NotFound();
+
+        return cliente;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+    {
+        cliente.DataCadastro = DateTime.UtcNow;
+
+        _context.Clientes.Add(cliente);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCliente), new { id = cliente.Id }, cliente);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+    {
+        if (id != cliente.Id)
+            return BadRequest("ID do corpo diferente do ID da URL.");
+
+        _context.Entry(cliente).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ClienteExists(id))
+                return NotFound();
+
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteCliente(int id)
+    {
+        var cliente = await _context.Clientes.FindAsync(id);
+        if (cliente == null)
+            return NotFound();
+
+        _context.Clientes.Remove(cliente);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ClienteExists(int id)
+    {
+        return _context.Clientes.Any(e => e.Id == id);
+    }
+}
