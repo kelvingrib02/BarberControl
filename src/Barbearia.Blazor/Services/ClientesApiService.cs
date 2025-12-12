4456fa40328ea5d279704fadaf14fe88f6c941f9
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-12 01:46:18 -0300

    Adicionando CRUD clientes

---
+++ src/Barbearia.Blazor/Services/ClientesApiService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using Barbearia.Blazor.Models;
+
+namespace Barbearia.Blazor.Services;
+
+public class ClientesApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public ClientesApiService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("Api");
+    }
+
+    public async Task<List<Cliente>> GetClientesAsync()
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<Cliente>>("api/clientes");
+        return result ?? [];
+    }
+}
