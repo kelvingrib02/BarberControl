170f84613baa0ec7facb00ed54d065dffecce803
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-12 01:23:30 -0300

    Adicionando swagger para testar

--- src/Barbearia.Api/Program.cs
+++ src/Barbearia.Api/Program.cs
@@ -10,7 +10,16 @@ builder.Services.AddDbContext<BarbeariaDbContext>(options =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
