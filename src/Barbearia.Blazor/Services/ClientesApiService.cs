037297d8605d7cc34093e7e0325d8cd85558c728
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-20 12:02:36 -0300

    Adicionando listagem de usuarios

--- src/Barbearia.Blazor/Services/ClientesApiService.cs
+++ src/Barbearia.Blazor/Services/ClientesApiService.cs
@@ -11,22 +11,23 @@ public class ClientesApiService
     {
         _httpClient = httpClient;
     }
-
-    public async Task CreateClienteAsync(Cliente cliente)
+    public async Task<List<Cliente>> GetClientesAsync()
     {
-        await _httpClient.PostAsJsonAsync("api/clientes", cliente);
+        var result = await _httpClient.GetFromJsonAsync<List<Cliente>>("api/clientes");
+        return result ?? new List<Cliente>();
     }
-
     public async Task<Cliente?> GetClienteByIdAsync(int id)
     {
         return await _httpClient.GetFromJsonAsync<Cliente>($"api/clientes/{id}");
     }
-
+    public async Task PostClienteAsync(Cliente cliente)
+    {
+        await _httpClient.PostAsJsonAsync("api/clientes", cliente);
+    }
     public async Task UpdateClienteAsync(Cliente cliente)
     {
         await _httpClient.PutAsJsonAsync($"api/clientes/{cliente.Id}", cliente);
     }
-
     public async Task DeleteClienteAsync(int id)
     {
         await _httpClient.DeleteAsync($"api/clientes/{id}");
