0185a41f58d0d2c4cf5b720819ad63b4c7cf73ca
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-23 22:27:04 -0300

    Iniciando barbeiros

---
+++ src/Barbearia.Api/Controller/BarbeirosController.cs
@@ -0,0 +1,23 @@
+﻿using Barbearia.Domain.Entities;
+using Barbearia.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barbearia.Api.Controllers
+
+[ApiController]
+[Route("api/[controller]")]
+public class BarbeirosController : ControllerBase
+{
+    private readonly BarbeariaDbContext _context;
+    public BarbeirosController(BarbeariaDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Barbeiro>>> GetBarbeiros()
+    {
+        return await _context.Barbeiros.ToListAsync();
+    }
+}
