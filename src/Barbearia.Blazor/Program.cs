ebfedb6a17f45d3213534e9bcc1e94ee7a96b895
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-12-26 15:36:26 -0300

    Continuando agendamentos

--- src/Barbearia.Blazor/Program.cs
+++ src/Barbearia.Blazor/Program.cs
@@ -17,6 +17,11 @@ builder.Services.AddHttpClient<BarbeirosApiService>(client =>
     client.BaseAddress = new Uri("https://localhost:7284");
 });
 
+builder.Services.AddHttpClient<AgendamentosApiService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7284");
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
---
+++ src/Barbearia.Blazor/Services/AgendamentosApiService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using Barbearia.Blazor.Models;
+
+namespace Barbearia.Blazor.Services
+{
+    public class AgendamentosApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AgendamentosApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Agendamento>> GetAgendamentosAsync()
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<Agendamento>>("api/agendamentos");
+            return result ?? new List<Agendamento>();
+        }
+
+        public async Task<Agendamento?> GetAgendamentoByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Agendamento>($"api/agendamentos/{id}");
+        }
+
+        public async Task PostAgendamentoAsync(Agendamento agendamento)
+        {
+            await _httpClient.PostAsJsonAsync("api/agendamentos", agendamento);
+        }
+
+        public async Task UpdateAgendamentoAsync(Agendamento agendamento)
+        {
+            await _httpClient.PutAsJsonAsync($"api/agendamentos/{agendamento.Id}", agendamento);
+        }
+
+        public async Task DeleteAgendamentoAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"api/agendamentos/{id}");
+        }
+    }
+}
