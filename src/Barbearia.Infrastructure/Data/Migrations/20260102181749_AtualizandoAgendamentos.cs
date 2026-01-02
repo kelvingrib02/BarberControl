63fe57b6e3c7c9b62de5939050dc31cc4fa98fa7
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-01-02 15:22:00 -0300

    Arrumando banco agendamentos

---
+++ src/Barbearia.Infrastructure/Data/Migrations/20260102181749_AtualizandoAgendamentos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barbearia.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoAgendamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataHoraFim",
+                table: "Agendamentos");
+
+            migrationBuilder.RenameColumn(
+                name: "DataHoraInicio",
+                table: "Agendamentos",
+                newName: "DataHora");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataHora",
+                table: "Agendamentos",
+                newName: "DataHoraInicio");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataHoraFim",
+                table: "Agendamentos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
